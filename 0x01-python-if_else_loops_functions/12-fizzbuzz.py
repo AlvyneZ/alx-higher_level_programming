8fc5365258585968a9e599e4153419ba2d7374f5
AlvyneZ <alvynew@gmail.com>
2023-03-07 23:44:03 +0300

    Rearranging spaces output

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1, 101):
-        if i == 1:
-            print("{}".format(i), end="")
-        elif i % 3 == 0:
-            print(" Fizz", end="")
+        if i % 3 == 0:
+            print("Fizz", end="")
             if i % 5 == 0:
-                print("Buzz", end="")
+                print("Buzz ", end="")
+            else:
+                print(" ", end="")
         elif i % 5 == 0:
-            print(" Buzz", end="")
+            print("Buzz ", end="")
         else:
-            print(" {}".format(i), end="")
-    print("")
+            print("{} ".format(i), end="")
