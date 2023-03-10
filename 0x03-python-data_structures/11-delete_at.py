85092f2358c8f0d4c0086fa3aa144a7672f94fed
AlvyneZ <alvynew@gmail.com>
2023-03-10 21:39:47 +0300

    Implementing list.pop()

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """My delete list element function
+
+    Args:
+        my_list: Original list with unwanted element
+        idx: Index of element to delete
+
+    Returns:
+        New list without unwanted element
+    """
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    cp = my_list.copy()
+    my_list.clear()
+    for i in range(len(cp)):
+        if i != idx:
+            my_list.append(cp[i])
+    return my_list
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_list = delete_at(my_list, idx)
+    print(new_list)
+    print(my_list)
