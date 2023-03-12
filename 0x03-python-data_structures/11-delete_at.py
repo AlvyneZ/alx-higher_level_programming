ccce05e6654fb48b3112194df6ec7182bf2ff172
AlvyneZ <alvynew@gmail.com>
2023-03-12 22:18:51 +0300

    returning list on indx error

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -10,7 +10,7 @@ def delete_at(my_list=[], idx=0):
         New list without unwanted element
     """
     if idx < 0 or idx > (len(my_list) - 1):
-        return None
+        return my_list
     cp = my_list.copy()
     my_list.clear()
     for i in range(len(cp)):
