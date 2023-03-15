36e2bf3b9d1906d58535feb978120b42ed8ce277
AlvyneZ <alvynew@gmail.com>
2023-03-15 19:48:01 +0300

    Making file 3 lines long

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,19 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """Function for multiplying values in a list with a given number
-
-    Args:
-        my_list: the list whose elements are to be updated
-        number: the number to multiply to the elements
-
-    Returns:
-        The new dictionary
-    """
     return list(map(lambda x: x * number, my_list))
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 6]
-    new_list = multiply_list_map(my_list, 4)
-    print(new_list)
-    print(my_list)
