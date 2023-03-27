f245e41363449077c70ca030fdecc4ef8219f389
AlvyneZ <alvynew@gmail.com>
2023-03-27 19:59:31 +0300

    adding value error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,6 +18,8 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except TypeError:
             print("wrong type")
+        except ValueError:
+            print("wrong type")
         except IndexError:
             print("out of range")
     return list_res
