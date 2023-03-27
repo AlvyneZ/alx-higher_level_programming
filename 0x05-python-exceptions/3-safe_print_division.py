d5f8a57f60a82cc27c754ce5bc62dbbf52a2b5ea
AlvyneZ <alvynew@gmail.com>
2023-03-27 19:33:19 +0300

    try except finally block

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Function for printing the division of two integers
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The result of the division
+    """
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        res = None
+    finally:
+        print("Inside result:{}".format(res))
+    return res
+
+
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
