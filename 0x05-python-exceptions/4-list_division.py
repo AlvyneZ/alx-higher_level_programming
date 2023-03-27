9a9e734ac0e224ff48ddcc73a09818afbd351673
AlvyneZ <alvynew@gmail.com>
2023-03-27 20:02:32 +0300

    Adding dummy finally

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -22,13 +22,15 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except IndexError:
             print("out of range")
+        finally:
+            pass
     return list_res
 
 
 if __name__ == "__main__":
     my_l_1 = [10, 8, 4]
     my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+    result = list_division(my_l_1, my_l_2, 0)
     print(result)
 
     print("--")
