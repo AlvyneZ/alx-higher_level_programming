44d1110f120be54eb4a531d828ae1a68df32a7c8
AlvyneZ <alvynew@gmail.com>
2023-03-28 18:29:24 +0300

    Minimal Empty class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """Empty class  that defines a square
+
+    Attrs:
+        None
+    """
+    pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
