1998ad611ee906784cba62c8bd572fdcebb19648
AlvyneZ <alvynew@gmail.com>
2023-04-03 22:57:30 +0300

    Reverting to try int conversion

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -16,10 +16,11 @@ def main():
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if argv[1].isdigit() is False:
+    try:
+        n = int(argv[1])
+    except ValueError:
         print("N must be a number")
         exit(1)
-    n = int(argv[1])
     if n < 4:
         print("N must be at least 4")
         exit(1)
