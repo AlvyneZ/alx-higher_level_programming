2f97c203279cb6f03399dea353c0723fd68ddafd
AlvyneZ <alvynew@gmail.com>
2023-04-04 22:48:47 +0300

    Removing the comma at the end

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.n = getattr(magic_string, "n", 1) + 1
-    return "BestSchool, " * (magic_string.n - 1)
+    return ", ".join(["BestSchool"] * (magic_string.n - 1))
 
 
 if __name__ == "__main__":
