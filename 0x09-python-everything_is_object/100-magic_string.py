8cc7e4f5d76e8c49f28347a8b24a947e7a7b6c81
AlvyneZ <alvynew@gmail.com>
2023-04-04 22:45:08 +0300

    Python function attributes to get static variables

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_string():
+    magic_string.n = getattr(magic_string, "n", 1) + 1
+    return "BestSchool, " * (magic_string.n - 1)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(magic_string())
