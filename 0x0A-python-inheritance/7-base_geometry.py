29b108511ac91881a2c118613eae9a45e1ecde1b
AlvyneZ <alvynew@gmail.com>
2023-04-11 21:56:33 +0300

    Allowing for classes derived from int

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -28,7 +28,7 @@ class BaseGeometry:
             name (string): name of the argument being validated
             value (integer): value provided
         """
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
