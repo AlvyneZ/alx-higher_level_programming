52b370c6d79fbf71f516b835df31eb1c3944f4bd
AlvyneZ <alvynew@gmail.com>
2023-04-11 20:31:25 +0300

    Derived class from List base class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module provides one class:
+    MyList(List)
+"""
+
+
+class MyList(list):
+    """
+    Class that adds sorted printing to lists
+
+    Attrs:
+        print_sorted(self): Prints elements of a list of integers
+         in ascending order
+    """
+
+    def __init__(self):
+        """
+        Initializer for initializing base class
+        """
+        super().__init__()
+
+    def print_sorted(self):
+        """
+        Prints elements of a list of integers in ascending order
+        """
+        sList = self.copy()
+        sList.sort()
+        print(sList)
