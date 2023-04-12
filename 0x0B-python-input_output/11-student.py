67147ec85de95c8af6fcbe4e7483cc2e33c25c66
AlvyneZ <alvynew@gmail.com>
2023-04-12 22:09:13 +0300

    Fixing pycodestyle

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -53,10 +53,11 @@ class Student:
 if __name__ == "__main__":
     import os
     import sys
-    
+
     read_file = __import__('0-read_file').read_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     path = sys.argv[1]
 
@@ -69,7 +70,9 @@ if __name__ == "__main__":
     print(student_1)
     print(type(student_1))
     print(type(j_student_1))
-    print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
+    print("{} {} {}".format(
+        student_1.first_name, student_1.last_name, student_1.age
+    ))
 
     save_to_json_file(j_student_1, path)
     read_file(path)
@@ -79,7 +82,9 @@ if __name__ == "__main__":
     new_student_1 = Student("Fake", "Fake", 89)
     print(new_student_1)
     print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    print("{} {} {}".format(
+        new_student_1.first_name, new_student_1.last_name, new_student_1.age
+    ))
 
     print("Load dictionary from file:")
     new_j_student_1 = load_from_json_file(path)
@@ -87,4 +92,6 @@ if __name__ == "__main__":
     new_student_1.reload_from_json(j_student_1)
     print(new_student_1)
     print(type(new_student_1))
-    print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
+    print("{} {} {}".format(
+        new_student_1.first_name, new_student_1.last_name, new_student_1.age
+    ))
