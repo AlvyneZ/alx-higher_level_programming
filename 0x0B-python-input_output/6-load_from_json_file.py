9f9361ab64285b0dbd5ac01c2ecbfac1227e4486
AlvyneZ <alvynew@gmail.com>
2023-04-12 20:22:36 +0300

    Loading objects from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+This "6-load_from_json_file.py" module provides one function:
+    load_from_json_file(filename)
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that retrieves an object from a JSON file
+
+    Args:
+        filename: name of the JSON file
+
+    Returns:
+        The object saved in the JSON file
+    """
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.loads(file.read())
+
+
+if __name__ == "__main__":
+    filename = "my_list.json"
+    my_list = load_from_json_file(filename)
+    print(my_list)
+    print(type(my_list))
+
+    filename = "my_dict.json"
+    my_dict = load_from_json_file(filename)
+    print(my_dict)
+    print(type(my_dict))
+
+    try:
+        filename = "my_set_doesnt_exist.json"
+        my_set = load_from_json_file(filename)
+        print(my_set)
+        print(type(my_set))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        filename = "my_fake.json"
+        my_fake = load_from_json_file(filename)
+        print(my_fake)
+        print(type(my_fake))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
