9247e9198afc0bab14dedd0e28b9a254e00a0535
AlvyneZ <alvynew@gmail.com>
2023-04-16 20:50:51 +0300

    Added integer validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -36,6 +36,32 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    def pos_int_validator(self, name, value):
+        """
+        Ensures a value for an argument "name" is a positive integer
+
+        Args:
+            name (string): name of the argument being validated
+            value (integer): value provided
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+
+    def pos_nz_int_validator(self, name, value):
+        """
+        Ensures a value for an argument "name" is a positive non-zero integer
+
+        Args:
+            name (string): name of the argument being validated
+            value (integer): value provided
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+
     @property
     def width(self):
         """
@@ -49,11 +75,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """
-        Sets the private instance attribute width
+        Sets the private instance attribute width with validation (>= 0)
 
         Args:
             width (integer): width of the rectangle instance
         """
+        self.pos_nz_int_validator("width", width)
         self.__width = width
 
     @property
@@ -69,11 +96,12 @@ class Rectangle(Base):
     @height.setter
     def height(self, height):
         """
-        Sets the private instance attribute height
+        Sets the private instance attribute height with validation (>= 0)
 
         Args:
             height (integer): height of the rectangle instance
         """
+        self.pos_nz_int_validator("height", height)
         self.__height = height
 
     @property
@@ -89,11 +117,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """
-        Sets the private instance attribute x
+        Sets the private instance attribute x with validation (> 0)
 
         Args:
             x (integer): Grid horizontal location of the rectangle instance
         """
+        self.pos_int_validator("x", x)
         self.__x = x
 
     @property
@@ -109,9 +138,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """
-        Sets the private instance attribute y
+        Sets the private instance attribute y with validation (> 0)
 
         Args:
             x (integer): Grid vertical location of the rectangle instance
         """
+        self.pos_int_validator("y", y)
         self.__y = y
