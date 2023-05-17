8848601fca2e5fcc1df26cda5689acc369a35d81
AlvyneZ <alvynew@gmail.com>
2023-05-17 22:38:05 +0300

    INNER JOIN and RIGHT JOIN

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- A script that lists all shows contained in hbtn_0d_tvshows and
+--  their corresponding genres if any
+SELECT `tv_shows`.`title`, `tv_genres`.`name`
+    FROM tv_show_genres
+    INNER JOIN tv_genres ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
+    RIGHT JOIN tv_shows ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
+    ORDER BY `tv_shows`.`title` ASC, `tv_show_genres`.`name` ASC;
