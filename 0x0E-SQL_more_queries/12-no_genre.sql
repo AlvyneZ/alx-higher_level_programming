86fe1e24639d50a3b55c6c9eee027599810c9b8e
AlvyneZ <alvynew@gmail.com>
2023-05-17 21:52:19 +0300

    comment fix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
--- A script that lists all shows contained in hbtn_0d_tvshows and
---  their corresponding genres if any
+-- A script that lists all shows contained in hbtn_0d_tvshows that
+--  lack a corresponding genre
 SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
     FROM tv_show_genres
     RIGHT JOIN tv_shows ON `tv_show_genres`.`show_id` = `tv_shows`.`id`
