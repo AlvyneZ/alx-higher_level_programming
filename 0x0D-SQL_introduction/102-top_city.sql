8491de67b5c873d20a023035e1d9d21f378db241
AlvyneZ <alvynew@gmail.com>
2023-05-17 19:27:05 +0300

    LIMIT and nested SELECT

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,9 @@
 -- A script that displays the top 3 of cities temperature during July and
 --  August ordered by temperature (descending)
-SELECT TOP 3 `city`, AVG(`value`) AS `avg_temp` FROM
-    (SELECT * FROM temperatures WHERE `month` >= 7 AND `month` <= 8)
+SELECT `city`, AVG(`value`) AS `avg_temp`
+    FROM (SELECT * FROM temperatures
+        WHERE `month` = 7 OR `month` = 8
+    ) AS JulyAug
     GROUP BY `city`
-    ORDER BY `avg_temp` DESC;
+    ORDER BY `avg_temp` DESC
+    LIMIT 3;
