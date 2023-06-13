5da572fbe07c371929c95b2ee913939fab44e954
AlvyneZ <alvynew@gmail.com>
2023-06-13 20:45:59 +0300

    semistandard fix - No useless Constructor

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
