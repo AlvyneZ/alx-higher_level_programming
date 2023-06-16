7111be2530eec84118a394c563dd31d020dbeb72
AlvyneZ <alvynew@gmail.com>
2023-06-16 18:51:14 +0300

    More MySQLdb and SQL statements

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module provides a function filter_states to
+ retrieve states from a MySQL DB that start with "N"
+"""
+from sys import argv
+import MySQLdb
+
+
+def filter_states(mysql_username, mysql_password, database_name):
+    """
+    Returns a tuple of tuples containing the data of each state
+    """
+    conn = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name, charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states " +
+        "WHERE name LIKE 'N%' " +
+        "ORDER BY id ASC"
+    )
+    query_rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return query_rows
+
+
+if __name__ == "__main__":
+    """
+    When running as a script, use command line arguments as
+     function inputs and print output to screen
+    """
+    n = len(argv)
+    if n != 4:
+        print(
+            "Usage: {} mysql_username ".format(argv[0]) +
+            "mysql_password database_name"
+        )
+    else:
+        states = filter_states(argv[1], argv[2], argv[3])
+        for state in states:
+            print(state)
