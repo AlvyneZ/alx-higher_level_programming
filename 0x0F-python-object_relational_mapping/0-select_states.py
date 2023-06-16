b10d743db29b17f2e92252cfd69c11e10bbb2baf
AlvyneZ <alvynew@gmail.com>
2023-06-16 06:53:41 +0300

    Running MySQLdb

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module provides a function select_states to
+ retrieve states from a MySQL DB
+"""
+from sys import argv
+import MySQLdb
+
+
+def select_states(mysql_username, mysql_password, database_name):
+    """
+    Returns a tuple of tuples containing the data of each state
+    """
+    conn = MySQLdb.connect(
+        host="127.0.0.1", port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name, charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    cur.close()
+    conn.close()
+    return query_rows
+
+
+if __name__ == "__main__":
+    """
+    When running as a script, use command line arguments as
+     function inputs and print output to screen
+    """
+    n = len(argv)
+    if n != 4:
+        print(
+            "Usage: {} mysql_username ".format(argv[0]) +
+            "mysql_password database_name"
+        )
+    else:
+        states = select_states(argv[1], argv[2], argv[3])
+        for state in states:
+            print(state)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Python - Object-relational mapping
+## Author: AlvyneZ
+This directory contains ALX projects related to Object Relational Mapping (ORM) in Python.
