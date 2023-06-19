9a067c69dd4ae557b4f7f73d0f1f6d5743af1da6
AlvyneZ <alvynew@gmail.com>
2023-06-19 21:40:39 +0300

    Using a single engine

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -22,6 +22,7 @@ def insert_city_state(
         ),
         pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     city = City(name=city_name, state=State(name=state_name))
@@ -29,19 +30,6 @@ def insert_city_state(
     session.commit()
     session.close()
     return city
-
-
-def create_tables(mysql_username, mysql_password, database_name):
-    """
-    Creates the tables in the database
-    """
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            mysql_username, mysql_password, database_name
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
 
 
 if __name__ == "__main__":
@@ -58,7 +46,6 @@ if __name__ == "__main__":
             "mysql_password database_name"
         )
     else:
-        create_tables(argv[1], argv[2], argv[3])
         insert_city_state(
             argv[1], argv[2], argv[3],
             "San Francisco", "California"
