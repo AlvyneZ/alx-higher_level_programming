7069732a89441f29fb8e4701549f588c274a3725
AlvyneZ <alvynew@gmail.com>
2023-06-19 21:39:19 +0300

    small bug fix

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -17,7 +17,7 @@ def insert_city_state(
     Returns the newly created city Object
     """
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
             mysql_username, mysql_password, database_name
         ),
         pool_pre_ping=True
@@ -36,7 +36,7 @@ def create_tables(mysql_username, mysql_password, database_name):
     Creates the tables in the database
     """
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@127.0.0.1/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
             mysql_username, mysql_password, database_name
         ),
         pool_pre_ping=True
