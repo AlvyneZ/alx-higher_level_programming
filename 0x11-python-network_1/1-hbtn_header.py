6288dd710c33a6d06c9340565405c330b2fda384
AlvyneZ <alvynew@gmail.com>
2023-06-30 23:32:44 +0300

    urllib accessing response headers

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module of a function to fetch a URL and retrieve a header value
+"""
+import urllib.request
+
+
+def get_header(url: str, header_var: str):
+    """
+    Fetches data from the provided url and prints the body
+    """
+    with urllib.request.urlopen(url) as response:
+        return response.headers.get(header_var)
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    header = get_header(argv[1], "X-Request-Id")
+    print(header)
