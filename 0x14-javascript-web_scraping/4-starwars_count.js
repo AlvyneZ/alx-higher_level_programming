5a1ac036db4c8b37b4a0f64113d43fae04ec63ff
AlvyneZ <alvynew@gmail.com>
2023-07-25 21:10:41 +0300

    Accounting for slash-terminated url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,8 @@
 const request = require('request');
 
 let apiUrl = process.argv[2];
-apiUrl = apiUrl.slice(0, apiUrl.lastIndexOf('/')) + '/people/18'
+if (apiUrl.slice(-1) == '/') apiUrl = apiUrl.slice(0, -1);
+apiUrl = apiUrl.slice(0, apiUrl.lastIndexOf('/')) + '/people/18';
 request(apiUrl, (err, response) => {
   if (err) {
     console.log(err);
