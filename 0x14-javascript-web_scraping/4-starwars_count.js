74ac92f948b1c44d120ee94de3a5070a7a696dbc
AlvyneZ <alvynew@gmail.com>
2023-07-25 21:16:31 +0300

    Fixing semistandard

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@
 const request = require('request');
 
 let apiUrl = process.argv[2];
-if (apiUrl.slice(-1) == '/') apiUrl = apiUrl.slice(0, -1);
+if (apiUrl.slice(-1) === '/') apiUrl = apiUrl.slice(0, -1);
 apiUrl = apiUrl.slice(0, apiUrl.lastIndexOf('/')) + '/people/18';
 request(apiUrl, (err, response) => {
   if (err) {
