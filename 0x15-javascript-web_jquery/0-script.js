be8b42e8651d3e062db233b340c3d121f48be9f6
AlvyneZ <alvynew@gmail.com>
2023-08-04 09:30:52 +0300

    Updating element colours with querySelector

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+// Updating the text color of the <header> element to red (#FF0000)
+//  using document.querySelector
+
+const header = document.querySelector('header');
+
+header.style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/README.md
@@ -0,0 +1,3 @@
+# Javascript Web JQuery
+## Author: AlvyneZ
+This directory contains ALX projects related to Web JQuery in Javascript
