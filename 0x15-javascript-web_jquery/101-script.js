163fe0d8d4cb3fb1d7095ac26f5d4ec97984c1f9
AlvyneZ <alvynew@gmail.com>
2023-08-04 10:01:16 +0300

    JQuery .ready function

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,16 @@
+// Adding, removing and clearing LI elements from a list when
+//  the user clicks
+
+$(document).ready(function(){
+  $('#add_item').on('click', function () {
+    const newItem = $('<li>').text('Item');
+
+    $('ul.my_list').append(newItem);
+  });
+  $('#remove_item').on('click', function () {
+    $('ul.my_list li:last-child').remove();
+  });
+  $('#clear_list').on('click', function () {
+    $('ul.my_list').empty();
+  });
+});
